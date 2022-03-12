56724b37577c7cd2e0f93c988af7ad703cddb2d2
pixisprite <ojhaaayush123@gmail.com>
2022-03-12 14:54:44 +0530

    First commit

---
+++ src/App/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header";
+import Tuner from "../Tuner";
+
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<Tuner />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/Tuner/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tuner = () => {
+	return <div>Tuner</div>;
+};
+
+export default Tuner;
