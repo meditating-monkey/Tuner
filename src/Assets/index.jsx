bf34c587c06b73ea9aca5159c7dc7fe1f2fa48a3
pixisprite <ojhaaayush123@gmail.com>
2022-03-15 21:16:11 +0530

    Adding icons and changing title

---
+++ src/Assets/index.jsx
@@ -0,0 +1,21 @@
+export function BackwardArrow(props) {
+	return (
+		<svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+			<path
+				fill="currentColor"
+				d="M17.77 3.77L16 2L6 12l10 10l1.77-1.77L9.54 12z"
+			></path>
+		</svg>
+	);
+}
+
+export function ForwardArrow(props) {
+	return (
+		<svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+			<path
+				fill="currentColor"
+				d="M6.23 20.23L8 22l10-10L8 2L6.23 3.77L14.46 12z"
+			></path>
+		</svg>
+	);
+}
